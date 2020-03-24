435699c2685df3fa7b87ec8443e6531c07a6e6b1
arcase6 <41893575+arcase6@users.noreply.github.com>
2020-03-24 13:30:01 +0900

    Changed to use indexes
    Correspongs to OpenGL Element Array Buffers.

--- OpenGLScratchPad/OpenGLScratchPad/CustomRenderer.cpp
+++ OpenGLScratchPad/OpenGLScratchPad/CustomRenderer.cpp
@@ -10,19 +10,35 @@ void CustomRenderer::Initialize()
 		+1.0f,1.0f,
 		-1.0f,1.0f,
 
-		+0.0f, +0.0f,
+		//+0.0f, +0.0f, //removed because redundant data
 		-1.0f,-1.0f,
 		+1.0f,-1.0f,
 	};
-	GLuint myBufferID;
+	GLuint vertexBufferID;
 	
 	//set up buffer and send vertex array to the array buffer
-	glGenBuffers(1, &myBufferID);
-	glBindBuffer(GL_ARRAY_BUFFER, myBufferID);
+	glGenBuffers(1, &vertexBufferID);
+	
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(verts),verts, GL_STATIC_DRAW);
 
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
+	
+	GLushort indices[] =
+	{
+		3,1,2,
+		0,3,4
+	};
+
+	GLuint indexBufferID;
+
+	//set up buffer and send vertex array to the array buffer
+	glGenBuffers(1, &indexBufferID);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferID);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
 
 	//glClearColor(1, 0, 0, 1);
 	//glClear(GL_COLOR_BUFFER_BIT);
@@ -33,5 +49,9 @@ void CustomRenderer::Initialize()
 void CustomRenderer::RenderFrame()
 {
 	//glViewport(0, 0,);
-	glDrawArrays(GL_TRIANGLES,0,6);
+	//glDrawArrays(GL_TRIANGLES,0,6);
+	
+	//draw 6 triangles, with indices from the element array buffer
+	//the indices are unsigned shorts. the offset into the index array is 0
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, 0);
 }
