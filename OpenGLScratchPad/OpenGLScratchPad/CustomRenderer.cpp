5a177c35e8a474eb3aa2aa9619e39dc4d261abb9
arcase6 <41893575+arcase6@users.noreply.github.com>
2020-03-24 14:04:05 +0900

    Initial setup for vertex color information
    Color information (all red) is now being passed through the array buffer.

--- OpenGLScratchPad/OpenGLScratchPad/CustomRenderer.cpp
+++ OpenGLScratchPad/OpenGLScratchPad/CustomRenderer.cpp
@@ -4,27 +4,37 @@
 
 void CustomRenderer::Initialize()
 {
-	GLfloat verts[] = 
+	GLfloat verts[] =
 	{
-		+0.0f, 0.0f,
+		+0.0f, 0.0f, //position
+		1.0f,0.0f,0.0f, //color
+		
 		+1.0f,1.0f,
+		1.0f,0.0f,0.0f,
+		
 		-1.0f,1.0f,
+		1.0f,0.0f,0.0f,
 
-		//+0.0f, +0.0f, //removed because redundant data
 		-1.0f,-1.0f,
+		1.0f,0.0f,0.0f,
+		
 		+1.0f,-1.0f,
+		1.0f,0.0f,0.0f,
 	};
 	GLuint vertexBufferID;
-	
+
 	//set up buffer and send vertex array to the array buffer
 	glGenBuffers(1, &vertexBufferID);
-	
-	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(verts),verts, GL_STATIC_DRAW);
 
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
-	
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(0); //attribute 0 is assumed by opengl to be position
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 5, 0);
+
+	glEnableVertexAttribArray(1); //attribute 1 is not assumed to be anything
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 5, (char *)(sizeof(GLfloat) * 2));
+
 	GLushort indices[] =
 	{
 		3,1,2,
@@ -50,7 +60,7 @@ void CustomRenderer::RenderFrame()
 {
 	//glViewport(0, 0,);
 	//glDrawArrays(GL_TRIANGLES,0,6);
-	
+
 	//draw 6 triangles, with indices from the element array buffer
 	//the indices are unsigned shorts. the offset into the index array is 0
 	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, 0);
