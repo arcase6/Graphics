e45292184dc73015561b68f55608c99e56a70540
arcase6 <41893575+arcase6@users.noreply.github.com>
2020-03-24 14:27:32 +0900

    Code refactoring to make more readable
    Moved some repeated code to protected functions.
    
    Split code into pieces based on what was being done.

--- OpenGLScratchPad/OpenGLScratchPad/CustomRenderer.cpp
+++ OpenGLScratchPad/OpenGLScratchPad/CustomRenderer.cpp
@@ -2,58 +2,62 @@
 #include <CustomRenderer.h>
 #include <glew.h>
 
+GLfloat verts[] =
+{
+	+0.0f, 0.0f, //position
+	1.0f,0.0f,0.0f, //color
+
+	+1.0f,1.0f,
+	1.0f,0.0f,0.0f,
+
+	-1.0f,1.0f,
+	1.0f,0.0f,0.0f,
+
+	-1.0f,-1.0f,
+	1.0f,0.0f,0.0f,
+
+	+1.0f,-1.0f,
+	1.0f,0.0f,0.0f,
+};
+
+GLushort indices[] =
+{
+	3,1,2, //triangle 1
+	0,3,4 //triangle 2
+};
+
 void CustomRenderer::Initialize()
 {
-	GLfloat verts[] =
-	{
-		+0.0f, 0.0f, //position
-		1.0f,0.0f,0.0f, //color
-		
-		+1.0f,1.0f,
-		1.0f,0.0f,0.0f,
-		
-		-1.0f,1.0f,
-		1.0f,0.0f,0.0f,
+	PassVertexData();
+	PassIndicesData();
+}
 
-		-1.0f,-1.0f,
-		1.0f,0.0f,0.0f,
-		
-		+1.0f,-1.0f,
-		1.0f,0.0f,0.0f,
-	};
-	GLuint vertexBufferID;
-
-	//set up buffer and send vertex array to the array buffer
-	glGenBuffers(1, &vertexBufferID);
-
-	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
+void CustomRenderer::PassVertexData()
+{
+	GenerateAndBindBuffer(GL_ARRAY_BUFFER);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
 
 	glEnableVertexAttribArray(0); //attribute 0 is assumed by opengl to be position
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 5, 0);
 
 	glEnableVertexAttribArray(1); //attribute 1 is not assumed to be anything
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 5, (char *)(sizeof(GLfloat) * 2));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 5, (char*)(sizeof(GLfloat) * 2));
+}
 
-	GLushort indices[] =
-	{
-		3,1,2,
-		0,3,4
-	};
+void CustomRenderer::PassIndicesData()
+{
+	GenerateAndBindBuffer(GL_ELEMENT_ARRAY_BUFFER);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+}
 
-	GLuint indexBufferID;
+void CustomRenderer::GenerateAndBindBuffer(GLenum targetBuffer)
+{
+	GLuint bufferID;
 
 	//set up buffer and send vertex array to the array buffer
-	glGenBuffers(1, &indexBufferID);
+	glGenBuffers(1, &bufferID);
 
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferID);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-
-	//glClearColor(1, 0, 0, 1);
-	//glClear(GL_COLOR_BUFFER_BIT);
-
-
+	glBindBuffer(targetBuffer, bufferID);
 }
 
 void CustomRenderer::RenderFrame()
--- OpenGLScratchPad/OpenGLScratchPad/CustomRenderer.h
+++ OpenGLScratchPad/OpenGLScratchPad/CustomRenderer.h
@@ -1,8 +1,15 @@
 #pragma once
+
+#include <glew.h>
+
 class CustomRenderer
 {
     public:
         virtual void Initialize();
         virtual void RenderFrame();
+    protected:
+        void PassIndicesData();
+        void PassVertexData();
+        void GenerateAndBindBuffer(GLenum targetBuffer);
 };
 
