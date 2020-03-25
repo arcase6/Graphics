de494f8a600839a3dc9cb62f184a18cc4ebbe43c
arcase6 <41893575+arcase6@users.noreply.github.com>
2020-03-25 17:04:23 +0900

    2 Triangles  - predepth buffer
    Added a second triangle to the render. Also tied the size of position data size, color data size, stride, and triangle count to 3 defines at the top of the file.

--- OpenGLScratchPad/OpenGLScratchPad/CustomRenderer.cpp
+++ OpenGLScratchPad/OpenGLScratchPad/CustomRenderer.cpp
@@ -3,22 +3,37 @@
 #include <glew.h>
 #include <ShaderLoader.h>
 
+#define POSITION_DATA_SIZE 3
+#define COLOR_DATA_SIZE 3
+
+#define TRIANGLE_COUNT 2
+
 GLfloat verts[] =
 {
-	+0.0f, 1.0f, //position
+	+0.0f, 1.0f,-0.5f, //position
 	1.0f,0.0f,0.0f, //color
 
-	1.0f, -1.0f,
+	1.0f, -1.0f,-0.5f,
 	0.0f,1.0f,0.0f,
 
-	-1.0f,-1.0f,
+	-1.0f,-1.0f,-0.5f,
 	0.0f,0.0f,1.0f,
+
+
+	+0.0f, -1.0f,0.0f, //position
+	1.0f,0.0f,0.0f, //color
+
+	1.0f, 1.0f, 0.0f,
+	1.0f,0.0f,0.0f,
+
+	-1.0f,1.0f, 0.0f,
+	1.0f,0.0f,0.0f,
 };
 
 GLushort indices[] =
 {
 	0,2,1, //triangle 1
-	//0,3,4 //triangle 2
+	3,4,5 //triangle 2
 };
 
 void CustomRenderer::Initialize()
@@ -36,11 +51,13 @@ void CustomRenderer::PassVertexData()
 	GenerateAndBindBuffer(GL_ARRAY_BUFFER);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
 
+	int stride = POSITION_DATA_SIZE + COLOR_DATA_SIZE;
+
 	glEnableVertexAttribArray(0); //attribute 0 is assumed by opengl to be position
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 5, 0);
+	glVertexAttribPointer(0, POSITION_DATA_SIZE, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * stride, 0);
 
 	glEnableVertexAttribArray(1); //attribute 1 is not assumed to be anything
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 5, (char*)(sizeof(GLfloat) * 2));
+	glVertexAttribPointer(1, COLOR_DATA_SIZE, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * stride, (char*)(sizeof(GLfloat) * POSITION_DATA_SIZE));
 }
 
 void CustomRenderer::PassIndicesData()
@@ -64,7 +81,9 @@ void CustomRenderer::RenderFrame()
 	//glViewport(0, 0,);
 	//glDrawArrays(GL_TRIANGLES,0,6);
 
+	int pointCount = 3 * TRIANGLE_COUNT;
+
 	//draw 6 triangles, with indices from the element array buffer
 	//the indices are unsigned shorts. the offset into the index array is 0
-	glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_SHORT, 0);
+	glDrawElements(GL_TRIANGLES, pointCount, GL_UNSIGNED_SHORT, 0);
 }
